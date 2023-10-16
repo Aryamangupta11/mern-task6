1b6c44ae9c9ef4d237f4ee30a77880149806ab98
Aryamangupta11 <142079873+Aryamangupta11@users.noreply.github.com>
2023-10-16 23:55:50 +0530

    Add files via upload

---
+++ resumedata.js
@@ -0,0 +1,98 @@
+let resumeData = {
+  "imagebaseurl":"https://github.com/Aryamangupta11",
+  "name": "Aryaman Gupta",
+  "role": "Frontend Developer and Data Scientist",
+  
+  
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+     
+      {
+        "name":"github",
+        "url":"https://github.com/Aryamangupta11",
+        "className":"fa fa-github"
+      },
+     
+    ],
+  "aboutme":"I am currently a pre-final year student at The VIT Institute of  Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address":"India",
+  "website":"https://github.com/Aryamangupta11",
+  "education":[
+    {
+      "UniversityName":"The Vellorre Insitute of  Technology",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"",
+      "YearOfPassing":"",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":""
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":""
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":""
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":""
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
